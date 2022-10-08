c560955a26ff63ba31cde676749b313665f7525c
Enea <jorgji.enea@gmail.com>
2022-10-09 01:04:52 +0200

    "just an edited to the variable"

--- 02_Type_System/primitive_data_types.py
+++ 02_Type_System/primitive_data_types.py
@@ -12,8 +12,10 @@ my_string = "Louis"
 my_int = 45
 my_float = 3.14
 my_bool = True
+just_a_random_var = "This is a random variable with numbers and so on!"
 
 print(my_string)
 print(my_int)
 print(my_float)
 print(my_bool)
+print(just_a_random_var)
